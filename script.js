1922daa3dfb94e78461f9942e0db3078881f422c
Davi <davilimasoares1@outlook.com>
2025-11-21 21:28:43 -0300

    i made the operations functions

---
+++ script.js
@@ -0,0 +1,18 @@
+let part1;
+let part2;
+let sign;
+
+function operate() {}
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+function mutiply(num1, num2) {
+    return num1 * num2;
+}
+function divide(num1, num2) {
+    return num1 / num2;
+}
