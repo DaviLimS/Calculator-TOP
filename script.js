485b8cd493675ee6b74e3a8d6645713bee5bfb08
Davi <davilimasoares1@outlook.com>
2025-11-21 21:51:24 -0300

    you did theswitch's  operate function

--- script.js
+++ script.js
@@ -2,7 +2,29 @@ let part1;
 let part2;
 let sign;
 
-function operate() {}
+function operate(num1, sign, num2) {
+    switch (sign) {
+        case "+":
+            add(num1, num2)
+            break;
+
+        case "-":
+            subtract(num1, num2)
+            break;
+
+        case "*":
+            mutiply(num1, num2)
+            break;
+
+        case "/":
+            divide(num1, num2)
+            break;
+
+        default:
+            alert("what the hell did you do??")
+            break;
+    }
+}
 
 function add(num1, num2) {
     return num1 + num2;
