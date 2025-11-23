68ee6aa492befd93ec36bb8a05c31da87e8b455e
Davi <davilimasoares1@outlook.com>
2025-11-23 01:02:58 -0300

    started to develop the equation indentifier

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 let current = [];
-let numbers = {};
+let numbers = [];
+let index = 0;
+
+let btn7 = document.querySelector("#seven")
+btn7.addEventListener(onclick, numbers(7), 'click')
+
+function numbers(btn) {
+    numbers[index] = current[0];
+    current = [];
+    current[0] = btn;
+    index++;
+}
+
 /*
 criar um código em que o usuario digita o número e este é colocado em um array quando ele começar a escrever um novo dígito, esse digito vai ser armazenado em um objeto e quando ele começar a escrever o segundo número, o array será limpo e será colocado o sinal nele, e este será armazenado no objeto quando o sinal de igual OU outro sinal for pressionado, o último número também sera armazenado no objeto quando o sinal de igual for pressionado.
 let sign
