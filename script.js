f6fe6494ed1ec85e6b2060f011bd1cf4b61b0f8f
Davi <davilimasoares1@outlook.com>
2025-11-23 22:07:57 -0300

    i created the first test of a button function

--- script.js
+++ script.js
@@ -2,15 +2,13 @@ let current = [];
 let numbers = [];
 let index = 0;
 
-let btn7 = document.querySelector("#seven")
-btn7.addEventListener(onclick, numbers(7), 'click')
-
-function numbers(btn) {
-    numbers[index] = current[0];
+let btn7 = document.getElementById('seven')
+btn7.addEventListener("click", function() {
     current = [];
-    current[0] = btn;
+    current[0] = 7;
+    numbers[index] = current[0];
     index++;
-}
+});
 
 /*
 criar um código em que o usuario digita o número e este é colocado em um array quando ele começar a escrever um novo dígito, esse digito vai ser armazenado em um objeto e quando ele começar a escrever o segundo número, o array será limpo e será colocado o sinal nele, e este será armazenado no objeto quando o sinal de igual OU outro sinal for pressionado, o último número também sera armazenado no objeto quando o sinal de igual for pressionado.
